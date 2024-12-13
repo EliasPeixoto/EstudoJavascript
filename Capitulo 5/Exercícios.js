d6c39b5bfd97631ead183a67944b9e3e2af23fca
Elias Peixoto <31219932+EliasPeixoto@users.noreply.github.com>
2024-12-13 13:18:04 -0300

    realização de todos os exercicios do capitulo 5.

--- Capitulo 5/Exercícios.js
+++ Capitulo 5/Exercícios.js
@@ -4,16 +4,37 @@ function manipulacaoObjeto()
     {
         nome:'Carlos',
         idade:23,
-        curso:'Matemática'
-    }
-    identificacaoEstudante(estudante);
+        curso:'Matemática',
+        identificar:function(){
+            console.log(`O aluno ${estudante.nome} tem ${estudante.idade} anos e cursa ${estudante.curso}.`);
+        },
+    };
+    estudante.identificar();
     estudante.nome='Fernanda';
     estudante.idade=27;
     estudante.curso='Geografia';
-    identificacaoEstudante(estudante);
+    estudante.identificar();
+    estudante.semestre = 'Terceiro';
+    console.log(estudante.semestre);
 }
-
-function identificacaoEstudante(estudante)
+function ManipulacaoArray()
 {
-    console.log(`O estudante de nome ${estudante.nome} tem ${estudante.idade} e está cursando ${estudante.curso}.`);
+    let frutas = ["Maçã","Pêra","Uva"];
+    console.log(frutas);
+    frutas.push("Mamão");
+    console.log(frutas);
+    let listaFrutas = frutas.join();
+    console.log(listaFrutas);
+    let duasFrutas = frutas.slice(1,3);
+    console.log(duasFrutas);
+}
+function interacaoArray()
+{
+    let numeros ={1,3,5,7,9};
+    function expo2(n)
+    {
+        return n*n;
+    }
+    let quadrados = numeros.map(expo2(numeros));
+    console.log(quadrados);
 }
