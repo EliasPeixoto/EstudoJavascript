a79e25e5559f93e4abad5211121a7494b8bb04b0
Elias Peixoto <31219932+EliasPeixoto@users.noreply.github.com>
2025-01-08 08:46:47 -0300

    Adição do campo de email

--- Capítulo 7/Exercícios.js
+++ Capítulo 7/Exercícios.js
@@ -2,6 +2,7 @@ let meuFormulario = document.getElementById("meuFormulario");
 let meuCheckbox = document.getElementById("meuCheckbox");
 let meuSelect = document.getElementById("meuSelect");
 let nome = document.getElementById("nome");
+let email = document.getElementById("email");
 
 meuFormulario.addEventListener("submit",(e)=>{
 
