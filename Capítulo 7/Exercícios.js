6f69616dcf93a96cb4cbd9734fa418852012e8e5
Elias Peixoto <31219932+EliasPeixoto@users.noreply.github.com>
2025-01-12 09:49:05 -0300

    Atualizacao do script exercicio.

--- Capítulo 7/Exercícios.js
+++ Capítulo 7/Exercícios.js
@@ -3,9 +3,11 @@ let meuCheckbox = document.getElementById("meuCheckbox");
 let meuSelect = document.getElementById("meuSelect");
 let nome = document.getElementById("nome");
 let email = document.getElementById("email");
+let senha = document.getElemeentById("senha");
 
 meuFormulario.addEventListener("submit",(e)=>{
 
+    alert(String.fromCharCode(65), String.fromCharCode(91));
     e.preventDefault();
     if(nome.value ==="")
     {
@@ -20,7 +22,6 @@ meuFormulario.addEventListener("submit",(e)=>{
     {
         alert("Por favor, insira um e-mail valido");
     }
-        
-    
+
 })
 
